1c51368222dcb7e5f6f608119d484179f675ce00
y530883536 <530883536@qq.com>
2019-01-27 18:54:54 +0800

    希望回家还能学得下去

--- src/main/java/basic/Jdk8New.java
+++ src/main/java/basic/Jdk8New.java
@@ -1,18 +1,45 @@
 package basic;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Jdk8New {
-    public static void main(String[] args) {
-        List<String> strList = new ArrayList<>();
-        strList.add("a");
-        strList.add("b");
-        strList.add("c");
+    public static void main(String[] args) throws Exception{
+        List<String> strList1 = new ArrayList<>();
+        strList1.add("a");
+        strList1.add("b");
+        strList1.add("c");
         //lambda表达式的写法 (参数) -> {代码}
-        strList.forEach((str) -> {
-            System.out.println("fuck");
-            System.out.println(str);
-        });
+        //strList1.forEach((str) -> System.out.println(str));
+        //上面这句的更简略写法，但是感觉会被人打死
+        strList1.forEach(System.out::println);
+        System.out.println("-----------------------");
+
+        //将字母变成大写
+        List<String> strList2 = new ArrayList<>();
+        strList2.add("fuck");
+        strList2.add("you");
+        strList2.add("man");
+        List<String> result1 = strList2.stream().map(String::toUpperCase).collect(Collectors.toList());
+        result1.forEach(str -> System.out.println(str));
+        System.out.println("-----------------------");
+
+        //将数字变成平方
+        List<Integer> intList1 = new ArrayList<>();
+        intList1.add(4);
+        intList1.add(6);
+        intList1.add(8);
+        List<Integer> result2 = intList1.stream().map(n -> n * 2).collect(Collectors.toList());
+        result2.forEach(i -> System.out.println(i));
+        System.out.println("-----------------------");
+
+        //将一片文章里面所有的单词存进一个List中
+        BufferedReader br = new BufferedReader(new FileReader("D:/article.txt"));
+        List<String> result3 = br.lines().flatMap(line -> Stream.of(line.split(" |[.]|,"))).collect(Collectors.toList());
+        System.out.println("cao");
     }
 }
--- src/main/java/basic/StringFormatTest.java
+++ src/main/java/basic/StringFormatTest.java
@@ -8,5 +8,10 @@ public class StringFormatTest {
     public static void main(String[] args) {
         String str1 = String.format("you are a %s man", "fucking");
         System.out.println(str1);
+
+        String str2 = "hello/world/fuck\\you\\man";
+        String[] split = str2.split("/|\\\\");
+        System.out.println(str2);
+        System.out.println("aaa");
     }
 }
