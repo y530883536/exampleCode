c39552ada568ee74250936f6b3743a76e4a41593
yechangfeng <yechangfeng@techb.top>
2019-03-01 17:00:44 +0800

    装饰模型

--- src/main/java/basic/MainApp.java
+++ src/main/java/basic/MainApp.java
@@ -1,10 +1,15 @@
 package basic;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.*;
 
 /**
  * 这个类是测试代码的地方，记得及时清掉测试过代码，并且不要删除这个类
@@ -12,16 +17,7 @@ import java.util.List;
  * @date 2018/12/20
  */
 public class MainApp {
-    public static void main(String[] args) {
-        List<Integer> intList1 = new ArrayList<>();
-        intList1.add(1);
-        intList1.add(2);
-        List<Integer> intList2 = Collections.synchronizedList(intList1);
-        intList2.add(3);
-        intList1.add(4);
-        intList1.set(0,11);
-        intList2.set(1,22);
-        System.out.println(intList1);
-        System.out.println(intList2);
+    public static void main(String[] args) throws Exception {
+
     }
 }
